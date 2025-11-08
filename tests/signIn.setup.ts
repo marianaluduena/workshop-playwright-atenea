4de957827fd6f553346a117376ae540c565719cd
Mariana Luduena <63405308+marianaluduena@users.noreply.github.com>
2025-11-08 18:38:21 -0300

    Add setup login user

--- tests/signIn.setup.ts
+++ tests/signIn.setup.ts
@@ -1,7 +1,9 @@
-import { test as setup} from "@playwright/test";
+import { test as setup } from "@playwright/test";
 import { LoginPage } from "../pages/loginPage";
 import { DashboardPage } from "../pages/dashboardPage";
 import { ModalCreateAccount } from "../pages/modalCreateAccount";
+import { BackendUtils } from "../utils/backendUtils";
+import TestData from "../data/testData.json";
 
 // johnny-soy-rico@fake.com will receive money
 
@@ -27,13 +29,15 @@ setup.beforeEach(async ({ page }) => {
 
 })
 
-// Login user that will send money
-/*
-setup("Login the user that will send money", async ({ page }) => {
+// Create user that will send money
 
-    await loginPage.loginUser("rick-sin-tierra@fake.com", "123456");
+setup("Create the user that will send money", async ({ page, request }) => {
+
+    const newUser = await BackendUtils.createUserViaAPI(request, TestData.validUser);
+    await loginPage.loginUser(newUser.email, newUser.password);
     await dashboardPage.addAccountBtn.click();
-    await modalCreateAccount.createAccount("Débito", "500");
+    await modalCreateAccount.createAccount("Débito", "1000");
+    await page.context().storageState({path: userSendsMoneyAuthFile});
 
 
     // Important: To capture the user's session, first the dashboard URL must be loaded 
@@ -41,14 +45,12 @@ setup("Login the user that will send money", async ({ page }) => {
 
     // Store user's session
 
-    await page.context().storageState({ path: userSendsMoneyAuthFile });
+    await page.context().storageState({path: userSendsMoneyAuthFile});
     await page.waitForTimeout(5000);
 
-})*/
-
-// Login the user that will receive money
+});
 /*
-setup("Create and login the user that will receive money", async ({ page, request: apiRequest }) => {
+setup("Login the user that will receive money", async ({ page, request: apiRequest }) => {
 
 
     // Ensure the user exists via API to avoid UI flakiness on CI
@@ -60,38 +62,21 @@ setup("Create and login the user that will receive money", async ({ page, reques
             email: 'johnny-soy-rico@fake.com',
             password: '123456'
         }
-    }).catch(() => {  });
+    }).catch(() => { });
 
     await loginPage.loginUser("johnny-soy-rico@fake.com", "123456");
     await page.waitForURL("http://localhost:3000/dashboard", { timeout: 60000 });
 
     // Store user's session
 
-    await page.context().storageState({path: userReceivesMoneyAuthFile });
+    await page.context().storageState({ path: userReceivesMoneyAuthFile });
     await page.waitForTimeout(2000);
 
 })*/
 
-setup("Create and login the user that will receive money", async ({ page, request: apiRequest }) => {
+setup("Login the user that will receive money", async ({ page}) => {
 
-
-    // Ensure the user exists via API to avoid UI flakiness on CI
-    await apiRequest.post('http://localhost:6007/api/auth/signup', {
-        headers: { 'Content-Type': 'application/json' },
-        data: {
-            firstName: 'Johnny',
-            lastName: 'Rico',
-            email: 'johnny-soy-rico@fake.com',
-            password: '123456'
-        }
-    }).catch(() => { /* ignore errors if user already exists */ });
-
-    await loginPage.loginUser("johnny-soy-rico@fake.com", "123456");
-    await page.waitForURL("http://localhost:3000/dashboard", { timeout: 60000 });
-
-    // Store user's session
-
-    await page.context().storageState({path: userReceivesMoneyAuthFile });
-    await page.waitForTimeout(2000);
+    await loginPage.loginUser("userReceivesMoney@gmail.com", "123456");
+    await page.context().storageState({path: userReceivesMoneyAuthFile});
 
 })
